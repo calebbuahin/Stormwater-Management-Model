3535fe29ea5fcbd5f537ca51ad16ea05c56f8c26
Caleb Buahin <calebbuahin@users.noreply.github.com>
2019-10-15 13:00:44 -0400

    Changed compiler flag to enable strict floating point operations #152, #153, and #207

---
+++ CMakeFiles/run-swmm.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/PROJECTCODE/Stormwater-Management-Model/src/swmm5.c" "C:/PROJECTCODE/Stormwater-Management-Model/CMakeFiles/run-swmm.dir/src/swmm5.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CLE=TRUE"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
