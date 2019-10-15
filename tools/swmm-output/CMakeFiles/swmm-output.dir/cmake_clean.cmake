8cb62004521332a28f41cc17ac98f1a161e18832
Caleb Buahin <calebbuahin@users.noreply.github.com>
2019-10-15 13:00:44 -0400

    Changed compiler flag to enable strict floating point operations #152, #153, and #207

---
+++ tools/swmm-output/CMakeFiles/swmm-output.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/swmm-output.dir/src/swmm_output.c.obj"
+  "CMakeFiles/swmm-output.dir/src/errormanager.c.obj"
+  "../../bin/swmm-output.pdb"
+  "../../bin/swmm-output.dll"
+  "../../lib/swmm-output.lib"
+  "../../bin/swmm-output.dll.manifest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/swmm-output.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
